23a25b3f869143fdbe9e9e9eb4511f0df181a66e
deg4uss3r <ricky@hosfelt.io>
2017-12-04 15:56:55 +0000

    Solution for day_4

--- 2017/day_4/src/main.rs
+++ 2017/day_4/src/main.rs
@@ -1,3 +1,29 @@
+//http://adventofcode.com/2017/day/4
+use std::collections::HashSet;
+
+fn valid_passphrase(input: String) -> bool {
+    let mut password_hash: HashSet<&str> = HashSet::new();
+    
+    let passphrase_list: Vec<&str> = input.split(' ').collect();
+    
+    for i in passphrase_list.iter() {
+        if password_hash.contains(i) {
+            return false;
+        }
+        else {
+            password_hash.insert(i);
+        }
+    }
+    true       
+}
+
 fn main() {
-    println!("Hello, world!");
+    //true
+    println!("{}",valid_passphrase("aa bb cc dd ee".to_string()));
+
+    //false
+    println!("{}",valid_passphrase("aa bb cc dd aa".to_string()));
+
+    //true
+    println!("{}",valid_passphrase("aa bb cc dd aaa".to_string()));
 }
---
+++ 2017/day_4/target/debug/day_4.d
@@ -0,0 +1,1 @@
+/Users/rthosfelt/code/advent/2017/day_4/target/debug/day_4: /Users/rthosfelt/code/advent/2017/day_4/src/main.rs
