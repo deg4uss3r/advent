4b8cd8eaeda85afd63aefce9b4f4992531799be7
Michael Recachinas <m.recachinas@gmail.com>
2019-01-13 10:57:23 +0000

    Simplify part 1 and part 2
    Thought I'd take a pass at trying to simplify it and use as many iterator chains as possible :)

--- 2018/day_1/src/main.rs
+++ 2018/day_1/src/main.rs
@@ -3,44 +3,36 @@ use std::error::Error;
 use std::io::stdin;
 use std::io::Read;
 
+
+
 fn compute_frequency(input: &str) -> Result<i64, Box<Error>> {
-    let split_freqs = input.split("\n");
-    let num_freqs: Vec<i64> = split_freqs
+    Ok(input.trim().split("\n")
         .map(|x| {
             x.parse::<i64>()
-                .expect(&format!("Error could not parse {} as i64", x))
-        })
-        .collect();
-
-    Ok(num_freqs.iter().fold(0, |accumulator, x| accumulator + x))
+             .expect(&format!("Error could not parse {} as i64", x))
+        }).sum())
 }
 
-fn compute_duplicate_frequency(input: &str) -> Result<i64, Box<Error>> {
-    let split_freqs = input.split("\n");
-    let num_freqs: Vec<i64> = split_freqs
+fn compute_duplicate_frequency(input: &str) -> Result<i64, String> {
+    let mut set = HashSet::new();
+    set.insert(0);
+    let result = input.trim().split("\n")
         .map(|x| {
             x.parse::<i64>()
                 .expect(&format!("Error could not parse {} as i64", x))
         })
-        .collect();
+        .cycle()
+        .scan(0, |a, b| {
+            *a += b;
+            Some(*a)
+        })
+        .filter(|element| !set.insert(*element))
+        .next();
 
-    let mut frequencies: HashSet<i64> = HashSet::new();
-    let mut freq_sum = 0;
-    let mut freq_index = 0;
-
-    while !frequencies.contains(&freq_sum) {
-        frequencies.insert(freq_sum); //insert our current sum after we check it is not in the list
-
-        if freq_index >= num_freqs.len() {
-            //loop around the frequencies if we made it to the end of the list
-            freq_index = 0;
-        }
-
-        freq_sum += num_freqs[freq_index]; //compute the new freq sum
-        freq_index += 1;
+    match result {
+        Some(r) => Ok(r),
+        None => Err(String::from("no dupes"))
     }
-
-    Ok(freq_sum)
 }
 
 fn main() -> Result<(), Box<Error>> {
